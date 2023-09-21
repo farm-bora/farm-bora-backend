354466618c541981ae0c33cf54e4df65356ac11e
Timothy Karani <kartimothy@gmail.com>
2023-09-21 04:02:20 +0300

    Added plant app

---
+++ plants/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    botanical_name = models.CharField(max_length=100, null=True)
+    details = models.CharField(null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
