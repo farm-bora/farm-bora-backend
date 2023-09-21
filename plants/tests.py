e72aef7c699193259c80328d6ebac798b32aff20
Timothy Karani <kartimothy@gmail.com>
2023-09-21 04:13:15 +0300

    Added plant tests

--- plants/tests.py
+++ plants/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
+from .models import Plant
+
 # Create your tests here.
+class PlantModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.plant = Plant.objects.create(
+            name='Maize',
+            botanical_name='Zea Mays',
+            details='Also called corn by our friends from the West'
+        )
+
+    def test_model_content(self) -> None:
+        self.assertEqual(self.plant.name, 'Maize')
+        self.assertEqual(self.plant.botanical_name, 'Zea Mays')
+        self.assertEqual(self.plant.details, 'Also called corn by our friends from the West')
