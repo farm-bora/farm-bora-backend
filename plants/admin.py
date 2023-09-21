046743d109c702fd301e32eaf2528279a5cf887a
Timothy Karani <kartimothy@gmail.com>
2023-09-21 04:09:05 +0300

    Added plant to admin

--- plants/admin.py
+++ plants/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Plant
 
 # Register your models here.
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'botanical_name',)
+
+
+admin.site.register(Plant, PlantAdmin)
