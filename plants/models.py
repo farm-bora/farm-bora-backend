1fbf5a0c4e195cee91a2b463351ebf97e9b01c28
Timothy Karani <kartimothy@gmail.com>
2023-10-20 17:09:47 +0300

    added plant disease

--- plants/models.py
+++ plants/models.py
@@ -7,6 +7,7 @@ class Plant(models.Model):
     botanical_name = models.CharField(max_length=100, null=True)
     details = models.TextField(null=True)
     image = models.TextField(null=True)
+    slug = models.CharField(max_length=100, unique=True, null=True)
 
     def __str__(self) -> str:
         return self.name
@@ -16,6 +17,7 @@ class Disease(models.Model):
     plant = models.ForeignKey(Plant, on_delete=models.CASCADE, related_name="diseases")
     name = models.CharField(max_length=255)
     details = models.TextField(null=True)
+    slug = models.CharField(max_length=100, unique=True, null=True)
 
     def __str__(self) -> str:
         return self.name
